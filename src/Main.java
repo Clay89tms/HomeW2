9b709ae150d2aec2b5ad42bee2940fa31d5654fd
Clay89tms <bboyClay89@gmail.com>
2022-09-25 21:41:02 +0300

    1) hello <USER>

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please write name <USER>: ");
+
+        String us = scan.nextLine();
+        System.out.println("Hello " + us + "!");
     }
 }
